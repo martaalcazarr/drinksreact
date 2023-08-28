02fe8e66073e86666162fcf1183dfbbe39b49eb5
martaalcazarr <martaalcazarredondo@gmail.com>
2023-08-28 00:08:07 -0400

    add categories

--- src/components/DrinkForm.jsx
+++ src/components/DrinkForm.jsx
@@ -1,6 +1,9 @@
-import { Container, Form, Row, Col } from "react-bootstrap"
+import { Button, Form, Row, Col } from "react-bootstrap"
+import useCategories from "../hooks/useCategories"
 
 const DrinkForm = () => {
+
+    const {categories} = useCategories()
   return (
     <Form>
         <Row>
@@ -29,8 +32,26 @@ const DrinkForm = () => {
                         name="category"
                     >
                         <option>- Select one category --</option>
+                        {categories.map(category => (
+                            <option
+                            key={category.strCategory}
+                            value={category.strCategory}>
+                                {category.strCategory}
+                            </option>
+                        ))}
                     </Form.Select>
                 </Form.Group>
+            </Col>
+        </Row>
+        <Row className="justify-content-end">
+            <Col md={3}>
+                <Button
+                    variant="secondary"
+                    className="text-uppercase w-100"
+
+                >
+                    Search drinks
+                </Button>
             </Col>
         </Row>
     </Form>
