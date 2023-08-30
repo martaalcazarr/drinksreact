3280e2877f8f0c0a2798cd9dfce138c5cf03f7f3
martaalcazarr <martaalcazarredondo@gmail.com>
2023-08-30 16:53:39 -0400

    add modal to show drink with recipe

--- src/components/DrinkModal.jsx
+++ src/components/DrinkModal.jsx
@@ -3,15 +3,40 @@ import useDrinks from "../hooks/useDrinks"
 
 const DrinkModal = () => {
 
-    const {modal, handleModalClick} =useDrinks()
+    const {modal, handleModalClick, recipe} =useDrinks()
+
+    const showIngredients = () => {
+        let ingredients = []
+        for(let i = 1; i < 16; i++){
+            if(recipe[`strIngredient${i}`]){
+                ingredients.push(
+                    <li>{recipe[`strIngredient${i}`]}
+                    {recipe[`strMeasure${i}`]}</li>
+                )
+            }
+        }
+        return ingredients
+    }
 
   return (
     <Modal 
         show={modal}
         onHide={handleModalClick}
     >
+        <Image 
+            src={recipe.strDrinkThumb}
+            alt={`Image from recipe ${recipe.strDrink}`}
+            />
+            <Modal.Header>
+                <Modal.Title>{recipe.strDrink}</Modal.Title>
+            </Modal.Header>
         <Modal.Body>
-            body
+            <div className="p-3">
+                <h2>Instructions</h2>
+                {recipe.strInstructions}
+                <h2>Ingredients and quantities</h2>
+                {showIngredients()}
+            </div>
         </Modal.Body>
     </Modal>
 
